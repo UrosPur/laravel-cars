c787f86463f40fad1f8187e36aba0432742ddbac
UrosPurtic <uros.purtic@gmail.com>
2018-04-04 21:18:05 +0200

    welcome blade chage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to Cars Project
                 </div>
 
                 <div class="links">
