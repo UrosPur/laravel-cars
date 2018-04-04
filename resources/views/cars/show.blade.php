68b85b5db663affaa99088d8cf2acaaa97ec346b
UrosPurtic <uros.purtic@gmail.com>
2018-04-04 21:53:13 +0200

    zadatak 7

--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -1,7 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: zeljko
- * Date: 4.4.18.
- * Time: 21.44
- */
+dd($cars);
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 })->name('about');
+
+Route::get('/cars', 'CarsController@index')->name('cars');
